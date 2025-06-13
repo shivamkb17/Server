9feefa3772dad725b89ef30a3e21c8ee5859b531
Catapultam Habeo <alex@catapult.am>
2025-06-12 21:23:33 -0500

    stuff

---
+++ common/repositories/progression_flags_repository.h
@@ -0,0 +1,13 @@
+#ifndef EQEMU_PROGRESSION_FLAGS_REPOSITORY_H
+#define EQEMU_PROGRESSION_FLAGS_REPOSITORY_H
+
+#include "../database.h"
+#include "../strings.h"
+#include "base/base_progression_flags_repository.h"
+
+class ProgressionFlagsRepository: public BaseProgressionFlagsRepository {
+public:
+
+};
+
+#endif //EQEMU_PROGRESSION_FLAGS_REPOSITORY_H

1ddb563b1b0a4f728a52840239437e72aa900f24
Catapultam Habeo <alex@catapult.am>
2025-06-22 10:59:53 -0500

    move #find character to correct database

--- zone/gm_commands/find/character.cpp
+++ zone/gm_commands/find/character.cpp
@@ -6,7 +6,7 @@ void FindCharacter(Client *c, const Seperator *sep)
 	if (sep->IsNumber(2)) {
 		const auto character_id = Strings::ToUnsignedInt(sep->arg[2]);
 
-		const auto& e = CharacterDataRepository::FindOne(content_db, character_id);
+		const auto& e = CharacterDataRepository::FindOne(database, character_id);
 		if (!e.id) {
 			c->Message(
 				Chat::White,
@@ -34,7 +34,7 @@ void FindCharacter(Client *c, const Seperator *sep)
 	const auto search_criteria = Strings::ToLower(sep->argplus[2]);
 
 	const auto& l = CharacterDataRepository::GetWhere(
-		content_db,
+		database,
 		fmt::format(
 			"LOWER(`name`) LIKE '%%{}%%' AND `name` NOT LIKE '%-deleted-%' ORDER BY `id` ASC LIMIT 50",
 			search_criteria

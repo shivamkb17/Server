b8746ef58ac94ef3b19cae160a65ebcd6813d636
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2025-06-25 14:49:55 -0400

    [Code] LoginServerList Global to Singleton Cleanup (#4941)

--- world/client.cpp
+++ world/client.cpp
@@ -94,7 +94,6 @@ std::vector<RaceClassAllocation> character_create_allocations;
 std::vector<RaceClassCombos> character_create_race_class_combos;
 
 extern ZSList zoneserver_list;
-extern LoginServerList loginserverlist;
 extern ClientList client_list;
 extern uint32 numclients;
 extern volatile bool RunLoops;
@@ -554,7 +553,7 @@ bool Client::HandleSendLoginInfoPacket(const EQApplicationPacket *app)
 			ServerLSPlayerJoinWorld_Struct* join =(ServerLSPlayerJoinWorld_Struct*)pack->pBuffer;
 			strcpy(join->key,GetLSKey());
 			join->lsaccount_id = GetLSID();
-			loginserverlist.SendPacket(pack);
+			LoginServerList::Instance()->SendPacket(pack);
 			safe_delete(pack);
 		}
 
@@ -1470,7 +1469,7 @@ bool Client::Process() {
 			ServerLSPlayerLeftWorld_Struct* logout =(ServerLSPlayerLeftWorld_Struct*)pack->pBuffer;
 			strcpy(logout->key,GetLSKey());
 			logout->lsaccount_id = GetLSID();
-			loginserverlist.SendPacket(pack);
+			LoginServerList::Instance()->SendPacket(pack);
 			safe_delete(pack);
 		}
 		LogInfo("Client disconnected (not active in process)");

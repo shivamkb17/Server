91e22086bc1fe53b989c43d2ed6d80225e630d30
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2025-06-25 15:38:24 -0400

    [Code] AdventureManager Global to Singleton Cleanup (#4931)
    * [Code] AdventureManager Global to Singleton Cleanup
    
    * Post merge fix
    
    ---------
    
    Co-authored-by: Chris Miles <akkadius1@gmail.com>

--- zone/main.cpp
+++ zone/main.cpp
@@ -105,7 +105,6 @@ QuestParserCollection *parse        = 0;
 ZoneEventScheduler    event_scheduler;
 WorldContentService   content_service;
 PlayerEventLogs       player_event_logs;
-EvolvingItemsManager  evolving_items_manager;
 
 const SPDat_Spell_Struct* spells;
 int32 SPDAT_RECORDS = -1;

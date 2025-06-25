1d684c64f1cb289cecff6311aeace0a36d7cbf4a
Fryguy <fryguy503@gmail.com>
2025-06-25 13:25:35 -0400

    [Quest API] Lua Client Scoped EnableTitleSets (#4914)
    Example usage:
    ```lua
    client:EnableTitleSet(reward.title_set)
    ```

--- zone/lua_client.h
+++ zone/lua_client.h
@@ -317,6 +317,7 @@ public:
 	int GetClientVersion();
 	uint32 GetClientVersionBit();
 	void SetTitleSuffix(const char *text);
+	void EnableTitleSet(uint32 title_set);
 	void SetAAPoints(int points);
 	int GetAAPoints();
 	int GetSpentAA();
